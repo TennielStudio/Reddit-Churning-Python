50066416d09a762a13db5116367ec93a7cec0cde
TennielTakenaka <tenniel.lynn@gmail.com>
2022-11-01 21:57:49 -0700

    Top yearly posts
    * Access top yearly posts
    * Organize data in a data frame

--- env/Code/churner.py
+++ env/Code/churner.py
@@ -7,9 +7,30 @@ reddit_read_only = praw.Reddit(client_id="",         # your client id
 
 subreddit = reddit_read_only.subreddit("churning")
 
-print("Display Name: ", subreddit.display_name)
+# print("Display Name: ", subreddit.display_name)
 
-print("Title: ", subreddit.title)
+# print("Title: ", subreddit.title)
 
-print("Description: ", subreddit.description)
- 
+# print("Description: ", subreddit.description)
+
+posts = subreddit.top("year")
+
+posts_dict = {"Title": [], "Post Text": [], "ID": [], "Score": [],
+              "Total Comments": [], "Post URL": []}
+
+for post in posts:
+    posts_dict["Title"].append(post.title)
+
+    posts_dict["Post Text"].append(post.selftext)
+
+    posts_dict["ID"].append(post.id)
+
+    posts_dict["Score"].append(post.score)
+
+    posts_dict["Total Comments"].append(post.num_comments)
+
+    posts_dict["Post URL"].append(post.url)
+
+top_yearly_posts = pd.DataFrame(posts_dict) # create a pandas data frame to store the top yearly posts
+print(top_yearly_posts)
+
